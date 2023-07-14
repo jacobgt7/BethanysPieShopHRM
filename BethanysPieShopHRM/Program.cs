4a5b24d817284bb094b1401d11a5ec7630eab70a
Jacob Thomas <jacgretho@gmail.com>
2023-07-14 15:18:01 -0500

    WIP

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+//byte numberOfEmployees = 300;
+
+const double interestRate = 0.07;
+
+string firstName = "Bethany";
+
+string emptyString = "";
+
+double ratePerHour = 12.34;
+int numberOfHoursWorked = 165;
+
+double currentMonthWage = ratePerHour * numberOfHoursWorked;
+Console.WriteLine(currentMonthWage);
+
+
+
