03ae1eb2fcebf64d1a78498e71561f1b116927b0
Jacob Thomas <jacgretho@gmail.com>
2023-07-20 15:04:01 -0500

    finished module 3

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,32 +1,75 @@
 ﻿
 
-Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+//Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
-Console.WriteLine("Please enter your name: ");
+//Console.WriteLine("Please enter your name: ");
 
-string name = Console.ReadLine();
+//string name = Console.ReadLine();
 
-int monthlyWage = 1234;
+//int monthlyWage = 1234;
 
-int months = 12, bonus = 1000;
+//int months = 12, bonus = 1000;
 
-bool isActive = true;
+//bool isActive = true;
 
-double rating = 99.25;
+//double rating = 99.25;
 
-//byte numberOfEmployees = 300;
+////byte numberOfEmployees = 300;
 
-const double interestRate = 0.07;
+//const double interestRate = 0.07;
 
-string firstName = "Bethany";
+//string firstName = "Bethany";
 
-string emptyString = "";
+//string emptyString = "";
 
-double ratePerHour = 12.34;
+//double ratePerHour = 12.34;
+//int numberOfHoursWorked = 165;
+
+//bool a;
+//int b;
+
+//double currentMonthWage = ratePerHour * numberOfHoursWorked;
+//Console.WriteLine(currentMonthWage);
+
+
+//int inMaxValue = int.MaxValue;
+//int intMinValue = int.MinValue;
+
+//char userSelection = 'a';
+//char upperVersion = char.ToUpper(userSelection);
+
+//bool isDigit = char.IsDigit(userSelection);
+
+//bool isLetter = char.IsLetter(userSelection);
+
+DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
+Console.WriteLine(hireDate);
+
+DateTime exitDate = new DateTime(2025, 12, 11);
+
+//DateTime invalidDate = new DateTime(2025, 15, 11);
+
+//DateTime startDate = hireDate.AddDays(15);
+//Console.WriteLine(startDate);
+
+//DateTime currentDate = DateTime.Now;
+//bool areWeInDst = currentDate.IsDaylightSavingTime();
+
+//DateTime startHour = DateTime.Now;
+//TimeSpan workTime = new TimeSpan(8, 35, 0);
+//DateTime endHour = startHour.Add(workTime);
+
+//Console.WriteLine(startHour);
+//Console.WriteLine(endHour);
+
 int numberOfHoursWorked = 165;
 
-double currentMonthWage = ratePerHour * numberOfHoursWorked;
-Console.WriteLine(currentMonthWage);
+long veryLongMonth = numberOfHoursWorked;
 
+double d = 123456789.0;
 
+int x = (int) d;
 
+int intVeryLongMonth = (int)veryLongMonth;
+
+Console.ReadLine();
