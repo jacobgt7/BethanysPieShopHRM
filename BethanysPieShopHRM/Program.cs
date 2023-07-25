5ea9d0979cafadd1bfdb984ccac38dadd9e453e0
Jacob Thomas <jacgretho@gmail.com>
2023-07-25 18:25:52 -0500

    WIP

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,75 +1,141 @@
 ﻿
+//int age = 23;
 
-//Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+//bool a = age == 23;
 
-//Console.WriteLine("Please enter your name: ");
+//Console.WriteLine("Age is 23:" + a);
 
-//string name = Console.ReadLine();
+//bool b = age > 23;
 
-//int monthlyWage = 1234;
+//bool c = (age >= 18) && (age <= 65);
+//Console.WriteLine("Age is between 18 and 65: " + c);
 
-//int months = 12, bonus = 1000;
+//int age1 = 16;
+//int age2 = 64;
+//bool d = (age1 >= 18) && (age2 <= 65);
+//Console.WriteLine("Age1 is greater than 18 AND age2 is less than 65: " + d);
+//bool e = (age1 >= 18) || (age2 <= 65);
+//Console.WriteLine("Age1 is greater than 18 OR age2 is less than 65: " + e);
 
-//bool isActive = true;
+//Console.WriteLine("Enter the age of the new candidate: ");
+//int age = int.Parse(Console.ReadLine());
 
-//double rating = 99.25;
-
-////byte numberOfEmployees = 300;
-
-//const double interestRate = 0.07;
-
-//string firstName = "Bethany";
-
-//string emptyString = "";
-
-//double ratePerHour = 12.34;
-//int numberOfHoursWorked = 165;
-
-//bool a;
-//int b;
-
-//double currentMonthWage = ratePerHour * numberOfHoursWorked;
-//Console.WriteLine(currentMonthWage);
+////if (age < 18)
+////{
+////    Console.WriteLine("Too young too apply");
+////    Console.WriteLine("Send email to candidate.");
+////}
+////else if (age > 65)
+////{
+////    Console.WriteLine("Sorry, the selected age is too old");
+////    Console.WriteLine("Send email to candidate.");
+////}
+////else
+////{
+////    Console.WriteLine("Great, you can now start with the application!");
 
 
-//int inMaxValue = int.MaxValue;
-//int intMinValue = int.MinValue;
+//    switch(age)
+//    {
+//        case < 18:
+//        case > 65:
+//            Console.WriteLine("Sorry, your age is not within the range we are looking for");
+//            break;
+//        case 23:
+//            Console.WriteLine("Wow, exactly what we're looking for!");
+//            break;
+//        default:
+//            Console.WriteLine("Great, you can now start with the application!");
+//            break;
+//    }
 
-//char userSelection = 'a';
-//char upperVersion = char.ToUpper(userSelection);
 
-//bool isDigit = char.IsDigit(userSelection);
 
-//bool isLetter = char.IsLetter(userSelection);
 
-DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
-Console.WriteLine(hireDate);
 
-DateTime exitDate = new DateTime(2025, 12, 11);
 
-//DateTime invalidDate = new DateTime(2025, 15, 11);
 
-//DateTime startDate = hireDate.AddDays(15);
-//Console.WriteLine(startDate);
+//DateTime today = DateTime.Now;
+//bool endOfMonthPaymentStarted = false;
 
-//DateTime currentDate = DateTime.Now;
-//bool areWeInDst = currentDate.IsDaylightSavingTime();
+//if (today.Date.Day == 20)
+//{
+//    Console.WriteLine("Please start end-of-month employee payments");
+//}
+//else if (today.Date.Day >= 25 && !endOfMonthPaymentStarted)
+//{
+//    Console.WriteLine("Payments will be late!");
+//}
+//else isn't required!
 
-//DateTime startHour = DateTime.Now;
-//TimeSpan workTime = new TimeSpan(8, 35, 0);
-//DateTime endHour = startHour.Add(workTime);
 
-//Console.WriteLine(startHour);
-//Console.WriteLine(endHour);
+Console.WriteLine("Choose the action you want to do: ");
+Console.WriteLine("1. Add employee");
+Console.WriteLine("2. Update employee");
+Console.WriteLine("3. Delete employee");
+Console.WriteLine("99. Exit application");
+string selectedAction = Console.ReadLine();
 
-int numberOfHoursWorked = 165;
+while (selectedAction != "99")
+{
+    switch (selectedAction)
+    {
+        case "1":
+            Console.WriteLine("Adding new employee...");
+            break;
+        case "2":
+            Console.WriteLine("Updating employee...");
+            break;
+        case "3":
+            Console.WriteLine("Deleting employee...");
+            break;
+        default:
+            Console.WriteLine("Invalid input");
+            break;
+    }
+    Console.WriteLine("Choose the action you want to do: ");
+    Console.WriteLine("1. Add employee");
+    Console.WriteLine("2. Update employee");
+    Console.WriteLine("3. Delete employee");
+    Console.WriteLine("99. Exit application");
+    selectedAction = Console.ReadLine();
 
-long veryLongMonth = numberOfHoursWorked;
+}
 
-double d = 123456789.0;
+Console.WriteLine("Closing application");
 
-int x = (int) d;
 
-int intVeryLongMonth = (int)veryLongMonth;
 
-Console.ReadLine();
+//Console.WriteLine("Enter a value: ");
+//int max = int.Parse(Console.ReadLine());
+//int i = 0;
+
+//while (i < max)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//int i = 10;
+
+//while (i > 0)
+//{
+//    Console.WriteLine(i);
+//    i--;
+//}
+
+//Console.WriteLine("Loop finished");
+
+//int i = 0;
+//int j = 0;
+
+//while (i < 10)
+//{
+//    while (j < 10)
+//    {
+//        Console.WriteLine("i: " + i + "   j: " + j);
+//        j++;
+//    }
+//    j = 0;
+//    i++;
+//}
